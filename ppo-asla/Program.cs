22a361f91baace2339634f7ae34e2e191e71ee18
cruelkratos <mailgarvsethi@gmail.com>
2025-05-18 13:17:10 +0530

    pagerank

--- ppo-asla/Program.cs
+++ ppo-asla/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
@@ -12,8 +13,12 @@ namespace ppo_asla
     class RunQueue {
         Queue<string> queue;
         WebCrawler w;
+        public Dictionary<string, List<string>> graph;
         public HashSet<string> urls;
+        string url;
         public RunQueue(string url) {
+            this.url = url;
+            graph = new Dictionary<string, List<string>>();
             queue = new Queue<string>();
             w = new WebCrawler();
             urls = new HashSet<string>();
@@ -24,7 +29,7 @@ namespace ppo_asla
                 await Task.Delay(190); // to avoid rate limit //use ache se 
                 var top = queue.Dequeue();
                 Console.WriteLine(top); // to see simultaneously
-                var list = await w.Driver(top);
+                var list = await this.w.Driver(top);
                 if ( list.Count == 0 || list == null) {
                     continue;
                 }
@@ -36,6 +41,58 @@ namespace ppo_asla
                 }
             }
         }
+        public async Task dfs(string URL, int depth = 5) {
+            if (depth == 0) {
+                return;
+            }
+            urls.Add(URL);
+            Console.WriteLine($"{URL}");
+            await Task.Delay(190);
+            var list = await this.w.Driver(URL);
+            if (list.Count == 0 || list == null) {
+                return;
+            }
+            if (!graph.ContainsKey(URL)) {
+                graph[URL] = new List<string>();
+            }
+            foreach (var l in list) {
+                if (l != null && l.Contains("iitr.ac.in")) {
+                    graph[URL].Add(l);
+                }
+                if (l != null && !urls.Contains(l)) {
+                    await dfs(l, depth - 1);
+                }
+            }
+        }
+
+        public Dictionary<string, double> PageRank(int iterations = 20, double damping = 0.85) {
+            var nodes = graph.Keys.ToHashSet();
+            foreach (var neighbors in graph.Values)
+                foreach (var node in neighbors)
+                    nodes.Add(node);
+
+            var N = nodes.Count;
+            var pr = nodes.ToDictionary(node => node, node => 1.0 / N);
+
+            for (int it = 0; it < iterations; it++) {
+                var newPr = nodes.ToDictionary(node => node, node => (1.0 - damping) / N);
+
+                foreach (var node in graph.Keys) {
+                    if (graph[node].Count == 0) continue;
+
+                    double share = pr[node] / graph[node].Count;
+                    foreach (var dest in graph[node]) {
+                        if (newPr.ContainsKey(dest))
+                            newPr[dest] += damping * share;
+                    }
+                }
+
+                pr = newPr;
+            }
+
+            return pr;
+        }
+
     }
     class WebCrawler{
         public async Task<List<string>> Driver(string url) {
@@ -91,9 +148,13 @@ namespace ppo_asla
         {
             Console.WriteLine("hello");
             var run = new RunQueue("https://iitr.ac.in/");
-            await run.bfs();
-            foreach(var link in run.urls) {
-                Console.WriteLine(link);
+            await run.dfs("https://iitr.ac.in/");
+            //foreach(var link in run.urls) {
+            //    Console.WriteLine(link);
+            //}
+            var matrix = run.PageRank();
+            foreach (var p in matrix) {
+                Console.Write($"{p.Key} => {p.Value}");
             }
         }
     }
